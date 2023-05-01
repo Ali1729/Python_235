b7dbf99de9d9a5863ab94955f4d7956298ae9580
Ali <alishareef3@gamil.com>
2023-04-30 20:27:55 -0600

    new files

---
+++ 01_Basics/Intro.py
@@ -0,0 +1,15 @@
+# to demonstrate Indentaion.
+
+def func1():
+ print("ali");
+ for i in range(6):
+        
+  print(i)
+        
+ for j in range(10):
+        
+  print(j)
+
+def func2():
+    func1()
+   

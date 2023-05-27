519f466678cca5f141027a58c5323bd91d930717
Ali <alishareef3@gamil.com>
2023-05-26 21:52:22 -0600

    error coming

--- Project_quizz/question_data.py
+++
@@ -1,3 +0,0 @@
-list1 = ["ILIKE","Case-insensitive alternative for LIKE.",
-         "ILIKE ANY","Case-insensitive alternative for LIKE ANY.",
-         "ABS","Returns the absolute value of a numeric expression."]
